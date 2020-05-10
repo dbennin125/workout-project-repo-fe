101efcc4e2a08b780b0804b50014ad079580a06c
Daniel Bennington <dbennington125@gmail.com>
2020-05-10 15:07:24 -0700

    start on CSS

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import './App.css'
 
 //the nav worked better at pointing were to go than 
 //just linking
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className="headerContainer">
                 <h1>Welcome to the Gym!</h1>
-                <nav>
+                <nav className="nav">
                    <a href="../">Home</a>
                    <p>
                     <a href="../workouts">WorkoutList</a>
--- src/WorkoutList.js
+++ src/WorkoutList.js
@@ -10,24 +10,32 @@ export default class WorkoutList extends Component {
     state = { workouts: [] }
     //fetching data from API we wrote
     componentDidMount =  async () => {
+        
+        this.setState({ loadingPage: true });
         const data = await getExercises(this.state)
         // console.log(this.state)
-        //this.state only needed data on line 16(which comes from line 13), 
+        //this.state only needed data in setState(which comes from line 13), 
         //not data.body which was originally put there.
+        this.setState({ loadingPage: false })
         this.setState({ workouts: data })
         // console.log(this.state.workouts)
+        //add loading state
     }
     
     render() {
+        const { workouts, loadingPage } = this.state
         return (
             <div className="workoutListContainer">
                 <h2 className="workoutTitle">WorkoutList:</h2>
     
-                {
-                    this.state.workouts.map(workout => {
+                {   
+                
+                    loadingPage
+                    ?<img src='https://s3.scoopwhoop.com/anj/loading/594155876.gif' alt='loading' />
+                    : workouts.map(workout => {
                         return<SingleItem key="newList" workout= {workout} />
-                        
-                    }) 
+                    })
+                      
                 }
                 
             </div>
