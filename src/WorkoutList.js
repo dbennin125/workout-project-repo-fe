0c5395278727b4a2ef9413e502cc129a3002a1c9
Daniel Bennington <dbennington125@gmail.com>
2020-05-10 13:25:42 -0700

    almost there

--- src/WorkoutList.js
+++ src/WorkoutList.js
@@ -7,23 +7,29 @@ import { getExercises } from './Api-Calls.js'
 
 export default class WorkoutList extends Component {
     //set state
-    state= { workouts: [] }
+    state = { workouts: [] }
     //fetching data from API we wrote
     componentDidMount =  async () => {
         const data = await getExercises(this.state)
-        this.setState({ workouts: data.body })
+        // console.log(this.state)
+        //this.state only needed data on line 16(which comes from line 13), 
+        //not data.body which was originally put there.
+        this.setState({ workouts: data })
+        // console.log(this.state.workouts)
     }
     
     render() {
         return (
-            <div>
-                <h2>WorkoutList:</h2>
+            <div className="workoutListContainer">
+                <h2 className="workoutTitle">WorkoutList:</h2>
+    
                 {
                     this.state.workouts.map(workout => {
-                        return<SingleItem workout= {workout} />
+                        return<SingleItem key="newList" workout= {workout} />
                         
                     }) 
                 }
+                
             </div>
         )
     }
--- src/WorkoutPage.js
+++ src/WorkoutPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import request from 'superagent';
+// import request from 'superagent';
 import SingleItem from './SingleItem.js'
+import { getExercise } from './Api-Calls.js'
 
 
 
@@ -10,18 +11,22 @@ state = {
     workoutId: null
 }
  async componentDidMount() {
-    const data = await request.get(`https://calm-escarpment-70276.herokuapp.com/exercises/${this.props.match.params.id}`);
-    this.setState({ workoutId: data.body })
+    //fetching data from api call with you id from the link on singleitem.js
+    const fetchedData = await getExercise(this.props.match.params.id);
+    //setting state to fetchedData
+    this.setState({ workoutId: fetchedData })
     // console.log(this.state.workoutId)
 }
 
     render() {
     //    console.log(this.state.workoutId)
+    const {workoutId} = this.state;
+    //practicing deconstructors
         return <div>
             <h2>
             {
-                this.state.workoutId    
-                ? <SingleItem workout={this.state.workoutId} />
+                workoutId
+                ? <SingleItem workout={workoutId} />
                 : <img src='https://s3.scoopwhoop.com/anj/loading/594155876.gif' alt='loading' />}
             
             </h2>
