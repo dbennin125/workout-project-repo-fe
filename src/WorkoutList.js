a9f11e071a6421e43acfbc64142581cae61186d1
Daniel Bennington <dbennington125@gmail.com>
2020-05-11 22:34:10 -0700

    new backened added

--- src/WorkoutList.js
+++ src/WorkoutList.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react'
 // import request from 'superagent';
 import SingleItem from './SingleItem';
 import { getExercises } from './Api-Calls.js'
+import request from 'superagent';
 
 
 
 export default class WorkoutList extends Component {
     //set state
-    state = { workouts: [], filter: '' }
+    state = { workouts: [], filter: '', typeList: [] }
     //fetching data from API we wrote
     componentDidMount =  async () => {
         
-        this.setState({ loadingPage: true });
+        // this.setState({ loadingPage: true });
         const data = await getExercises(this.state)
+        const type = await request.get('https://mysterious-chamber-18797.herokuapp.com/types')
         // console.log(this.state)
+        // this.setState({ typeList: type.body })
         //this.state only needed data in setState(which comes from line 13), 
         //not data.body which was originally put there.
-        this.setState({ loadingPage: false })
-        this.setState({ workouts: data })
+        // this.setState({ loadingPage: false })
+        this.setState({ 
+            workouts: data,
+            typeList: type.body  })
         // console.log(this.state.workouts)
         //add loading state
     }
@@ -26,28 +31,27 @@ export default class WorkoutList extends Component {
         this.setState({ filter: e.target.value })
     }
     render() {
-        console.log(this.state.filter);
-        const { workouts, loadingPage } = this.state
+        console.log(this.state.typeList, this.state.filter);
+        const { filter, workouts, typeList, loadingPage } = this.state
         return (
             <div className="workoutListContainer">
                 <h2>WorkoutList:</h2>
                 <select onChange={this.handleFilter}>
-                    <option value="">Choose</option>
-                    <option value="resistance">Res</option>
-                    <option value="Cardio">Car</option>
-                    <option value="calastenticse">calastentics</option>
+                    <option  value="">Choose</option>
+                    {
+                        typeList.map(
+                            type => <option value={type.type}>{type.type}</option> ) 
+                    }
                 
                 </select>
     
                 {   
-                
                 loadingPage
                 ?<img src='https://s3.scoopwhoop.com/anj/loading/594155876.gif' alt='loading' />
-                : workouts
+                :workouts
                 .filter(workout => {
-                    if(!workouts.filter) return true;
-                    return workout.type === workouts.filter
-                })
+                    if (!filter) return true;
+                    return workout.type === filter})
                 .map(workout => {
                     return<SingleItem key="newList" workout= {workout} />
                 })
